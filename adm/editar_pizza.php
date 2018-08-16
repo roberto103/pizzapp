fdeb4963c91dd8e9fd5b6590cd253a07bf0a2b91
MatteuSilva <42352998+MatteuSilva@users.noreply.github.com>
2018-08-16 13:02:06 -0300

    pagina de editar pizza

--- adm/editar_pizza.php
+++ adm/editar_pizza.php
@@ -1,4 +1,32 @@
+<?php 
+	require_once 'core/consultas.php';
+ ?>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/fontawesome.css" integrity="sha384-GVa9GOgVQgOk+TNYXu7S/InPTfSDTtBalSgkgqQ7sCik56N9ztlkoTr2f/T44oKV" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/solid.css" integrity="sha384-Rw5qeepMFvJVEZdSo1nDQD5B6wX0m7c5Z/pLNvjkB14W6Yki1hKbSEQaX9ffUbWe" crossorigin="anonymous">
+
+<div class="container-fluid">
+      <div class="row" >
+        <div class="col-sm-12" style="background-color: #ff8a00 !important; height: 60px; position: absolute; z-index: 9999;">
+        </div> 
+      </div>
+      <div class="row">
+         <div class="col-sm-12" style="height: 100px; background-color: #343a40; color: white;">
+          <div style="float: left;">
+            <span>
+              <i class="fas fa-user-alt" style="margin-top: 70px;"></i>
+            </span>
+          </div>
+          <h6 style="margin-top: 68px; display: inline-block; margin-left: 15px; text-transform: capitalize;">Olá, <?php echo $_SESSION['nome_adm']; ?>!</h6>
+          <div style="display: inline-block; float: right;">
+            <a href="core/doLogout.php">
+              <i class="fas fa-sign-out-alt" style="margin-top: 70px;"></i>
+            </a>
+          </div>
+        </div>
+      </div>
 
 <div class="container-fluid">
   <div class="row">
