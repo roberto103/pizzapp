d995bd8392a8523da78ad4b45b7caa8bafd466db
Roberto <roberto10331@gmail.com>
2018-08-17 12:31:29 -0300

    Erro ao salvar atualização de promoção resolvido

--- adm/salvar_editar_promocao.php
+++ adm/salvar_editar_promocao.php
@@ -14,14 +14,14 @@
 		$data = date('Y-m-d H:i:s');
 
 		// Atualiza os dados no banco de dados
-		$sql = $pdo->prepare("UPDATE promocoes SET titulo = :titulo, desc_promo = :desc_promo, preco_promo = :preco_promo,duracao_promo = duracao, img_promo = :img, data = :data WHERE id = :id");
+		$sql = $pdo->prepare('UPDATE promocoes SET titulo = :titulo, desc_promo = :desc_promo, preco_promo = :preco_promo, duracao_promo = :duracao, img_promo = :img, data = :data WHERE id = :id');
 
 		$sql->bindValue(':titulo', $_POST['txtTitulo']);
 		$sql->bindValue(':desc_promo', $_POST['txtDescricao']);
 		$sql->bindValue(':preco_promo', decimalBanco($_POST['txtPreco']));
 		$sql->bindValue(':img', $novo_nome);
 		$sql->bindValue(':duracao', $_POST['txtDuracao']);
-		$sql->bindValue('data', $data);
+		$sql->bindValue(':data', $data);
 		$sql->bindValue(':id', $_POST['id']);
 		$sql->execute();
 		header('location:index.php');
