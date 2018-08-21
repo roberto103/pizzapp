3f4e23648993bf6068cfef5489a3850d1c9f022a
Roberto <roberto10331@gmail.com>
2018-08-21 12:54:40 -0300

    PagSeguro

--- checkout.php
+++ checkout.php
@@ -1,156 +1,156 @@
 <?php 
 
-  require_once 'core/Sessao.php';
+	require_once 'core/Sessao.php';
 
-  if (!Sessao::estaLogado()) {
-    header('Location: login.php');
-  }
+	if (!Sessao::estaLogado()) {
+		header('Location: login.php');
+	}
 
  ?>
  <!doctype html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="theme-color" content="#bd2130">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="img/favicon.ico">
-    <title>Checkout</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="css/form-validation.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/solid.css" integrity="sha384-wnAC7ln+XN0UKdcPvJvtqIH3jOjs9pnKnq9qX68ImXvOGz2JuFoEiCjT8jyZQX2z" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css" integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
-    <style type="text/css">
-      h4{color: white;}label{color: white;}
-    </style>
-    <link rel="stylesheet" type="text/css" href="sidebar_includes/css/b4_sidebar.css">
+	<head>
+		<meta charset="utf-8">
+		<meta name="theme-color" content="#bd2130">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="icon" href="img/favicon.ico">
+		<title>Checkout</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link href="css/form-validation.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/solid.css" integrity="sha384-wnAC7ln+XN0UKdcPvJvtqIH3jOjs9pnKnq9qX68ImXvOGz2JuFoEiCjT8jyZQX2z" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css" integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
+		<style type="text/css">
+			h4{color: white;}label{color: white;}
+		</style>
+		<link rel="stylesheet" type="text/css" href="sidebar_includes/css/b4_sidebar.css">
 
-  </head>
+	</head>
 
-  <body>
+	<body>
 
-    <div class="container-fluid">
-      <div class="row" >
-        <nav class="navbar navbar-expand-lg navbar-light bg-danger sidebarNavigation" data-sidebarClass="navbar-light bg-danger" style="width: 100%; position: absolute; z-index: 9999; background-color: #bd2130 !important;">
-              <button class="navbar-toggler leftNavbarToggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarMenu">
-                <div class="navbar-nav">
-                  <a class="nav-item nav-link" href="index.php" style="color: white;"><i class="fas fa-home"></i> Home</a>
-                  <a class="nav-item nav-link" href="cardapio.php" style="color: white;"><i class="fas fa-book-open"></i> Cardápio</a>
-                  <a class="nav-item nav-link" href="promocoes.php" style="color: white;"><i class="fas fa-dollar-sign"></i> Promoções</a>
-                  <a class="nav-item nav-link" href="cart.php" style="color: white;"><i class="fas fa-shopping-cart"></i> Meu Pedido</a>
-                  <a class="nav-item nav-link" href="entrega.php" style="color: white;"><i class="fas fa-map-marker-alt"></i> Local de Entrega</a>
-                  <a class="nav-item nav-link" href="conta.php" style="color: white;"><i class="fas fa-user-alt"></i> Minha conta</a>
-                  <a class="nav-item nav-link" href="critica.php" style="color: white;"><i class="fas fa-comment-alt"></i> Criticas e sugestões</a>
-                  <a class="nav-item nav-link" href="tel:08136341122" style="color: white;"><i class="fas fa-phone"></i> Pedir por telefone</a>
-                </div>
-             </div>
-        </nav>
-      </div> <!-- /navbar -->
+		<div class="container-fluid">
+			<div class="row" >
+				<nav class="navbar navbar-expand-lg navbar-light bg-danger sidebarNavigation" data-sidebarClass="navbar-light bg-danger" style="width: 100%; position: absolute; z-index: 9999; background-color: #bd2130 !important;">
+							<button class="navbar-toggler leftNavbarToggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+								<span class="navbar-toggler-icon"></span>
+							</button>
+							<div class="collapse navbar-collapse" id="navbarMenu">
+								<div class="navbar-nav">
+									<a class="nav-item nav-link" href="index.php" style="color: white;"><i class="fas fa-home"></i> Home</a>
+									<a class="nav-item nav-link" href="cardapio.php" style="color: white;"><i class="fas fa-book-open"></i> Cardápio</a>
+									<a class="nav-item nav-link" href="promocoes.php" style="color: white;"><i class="fas fa-dollar-sign"></i> Promoções</a>
+									<a class="nav-item nav-link" href="cart.php" style="color: white;"><i class="fas fa-shopping-cart"></i> Meu Pedido</a>
+									<a class="nav-item nav-link" href="entrega.php" style="color: white;"><i class="fas fa-map-marker-alt"></i> Local de Entrega</a>
+									<a class="nav-item nav-link" href="conta.php" style="color: white;"><i class="fas fa-user-alt"></i> Minha conta</a>
+									<a class="nav-item nav-link" href="critica.php" style="color: white;"><i class="fas fa-comment-alt"></i> Criticas e sugestões</a>
+									<a class="nav-item nav-link" href="tel:08136341122" style="color: white;"><i class="fas fa-phone"></i> Pedir por telefone</a>
+								</div>
+						 </div>
+				</nav>
+			</div> <!-- /navbar -->
 
-      <div class="row" id="cart">
+			<div class="row" id="cart">
 
-        <div class="col"></div>
-        <div class="col-md-8">
+				<div class="col"></div>
+				<div class="col-md-8">
 
-          <h4 class="mb-3">Pagamento</h4>
-          <form class="needs-validation" novalidate>
+					<h4 class="mb-3">Pagamento</h4>
+					<form class="needs-validation" action="pagseguro/pay.php" novalidate>
 
-            <div class="d-block my-3">
-              <div class="custom-control custom-radio">
-                <input id="credito" name="paymentMethod" type="radio" class="custom-control-input" checked required>
-                <label class="custom-control-label" for="credito">Cartão de crédito</label>
-              </div>
-              <div class="custom-control custom-radio">
-                <input id="debito" name="paymentMethod" type="radio" class="custom-control-input" required>
-                <label class="custom-control-label" for="debito">Cartão de débito</label>
-              </div>
-              <div class="custom-control custom-radio">
-                <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
-                <label class="custom-control-label" for="paypal">PayPal</label>
-              </div>
-            </div>
+						<div class="d-block my-3">
+							<div class="custom-control custom-radio">
+								<input id="credito" name="paymentMethod" type="radio" class="custom-control-input" checked required>
+								<label class="custom-control-label" for="credito">Cartão de crédito</label>
+							</div>
+							<div class="custom-control custom-radio">
+								<input id="debito" name="paymentMethod" type="radio" class="custom-control-input" required>
+								<label class="custom-control-label" for="debito">Cartão de débito</label>
+							</div>
+							<div class="custom-control custom-radio">
+								<input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
+								<label class="custom-control-label" for="paypal">PayPal</label>
+							</div>
+						</div>
 
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <label for="cc-nome">Nome no cartão</label>
-                <input type="text" class="form-control" id="cc-nome" placeholder="" required>
-                <small class="text-muted">Nome completo, como mostrado no cartão.</small>
-                <div class="invalid-feedback">
-                  O nome que está no cartão é obrigatório.
-                </div>
-              </div>
-              <div class="col-md-6 mb-3">
-                <label for="cc-numero">Número do cartão de crédito</label>
-                <input type="text" class="form-control" id="cc-numero" placeholder="" required>
-                <div class="invalid-feedback">
-                  O número do cartão de crédito é obrigatório.
-                </div>
-              </div>
-            </div>
+						<div class="row">
+							<div class="col-md-6 mb-3">
+								<label for="cc-nome">Nome no cartão</label>
+								<input type="text" class="form-control" id="cc-nome" placeholder="" required>
+								<small class="text-muted">Nome completo, como mostrado no cartão.</small>
+								<div class="invalid-feedback">
+									O nome que está no cartão é obrigatório.
+								</div>
+							</div>
+							<div class="col-md-6 mb-3">
+								<label for="cc-numero">Número do cartão de crédito</label>
+								<input type="text" class="form-control" id="cc-numero" placeholder="" required>
+								<div class="invalid-feedback">
+									O número do cartão de crédito é obrigatório.
+								</div>
+							</div>
+						</div>
 
-            <div class="row">
-              <div class="col-md-3 mb-3">
-                <label for="cc-expiracao">Data de expiração</label>
-                <input type="text" class="form-control" id="cc-expiracao" placeholder="" required>
-                <div class="invalid-feedback">
-                  Data de expiração é obrigatória.
-                </div>
-              </div>
-              <div class="col-md-3 mb-3">
-                <label for="cc-cvv">CVV</label>
-                <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-                <div class="invalid-feedback">
-                  Código de segurança é obrigatório.
-                </div>
-              </div>
-            </div>
+						<div class="row">
+							<div class="col-md-3 mb-3">
+								<label for="cc-expiracao">Data de expiração</label>
+								<input type="text" class="form-control" id="cc-expiracao" placeholder="" required>
+								<div class="invalid-feedback">
+									Data de expiração é obrigatória.
+								</div>
+							</div>
+							<div class="col-md-3 mb-3">
+								<label for="cc-cvv">CVV</label>
+								<input type="text" class="form-control" id="cc-cvv" placeholder="" required>
+								<div class="invalid-feedback">
+									Código de segurança é obrigatório.
+								</div>
+							</div>
+						</div>
 
-            <hr class="mb-4">
+						<hr class="mb-4">
 
-            <button class="btn btn-success btn-lg btn-block mb-2" type="submit" style="border-radius: 0;">Continue o checkout</button>
+						<button class="btn btn-success btn-lg btn-block mb-2" type="submit" style="border-radius: 0;">Continue o checkout</button>
 
-          </form>
+					</form>
 
-        </div>
-        <div class="col"></div>
+				</div>
+				<div class="col"></div>
 
-      </div>
+			</div>
 
-    </div>
+		</div>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
-    <script src="sidebar_includes/js/b4_sidebar.js"></script>
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+		<script src="sidebar_includes/js/b4_sidebar.js"></script>
 
-    <script>
-      // Exemplo de JavaScript para desativar o envio do formulário, se tiver algum campo inválido.
-      (function() {
-        'use strict';
+		<script>
+			// Exemplo de JavaScript para desativar o envio do formulário, se tiver algum campo inválido.
+			(function() {
+				'use strict';
 
-        window.addEventListener('load', function() {
-          // Selecione todos os campos que nós queremos aplicar estilos Bootstrap de validação customizados.
-          var forms = document.getElementsByClassName('needs-validation');
+				window.addEventListener('load', function() {
+					// Selecione todos os campos que nós queremos aplicar estilos Bootstrap de validação customizados.
+					var forms = document.getElementsByClassName('needs-validation');
 
-          // Faz um loop neles e previne envio
-          var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
-              if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-              }
-              form.classList.add('was-validated');
-            }, false);
-          });
-        }, false);
-      })();
-    </script>
+					// Faz um loop neles e previne envio
+					var validation = Array.prototype.filter.call(forms, function(form) {
+						form.addEventListener('submit', function(event) {
+							if (form.checkValidity() === false) {
+								event.preventDefault();
+								event.stopPropagation();
+							}
+							form.classList.add('was-validated');
+						}, false);
+					});
+				}, false);
+			})();
+		</script>
 
-  </body>
+	</body>
 </html>
