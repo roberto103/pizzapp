d4782d0b895200b7f7a297b562ad37c0a82484c5
roberto103 <roberto10331@gmail.com>
2018-08-22 18:13:16 -0300

    JavaScript do Bootstrap adicionado

--- index.php
+++ index.php
@@ -1,67 +1,68 @@
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
-    <meta charset="utf-8">
-    <meta name="theme-color" content="#bd2130">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title>Pizzapp</title>
-    <link rel="shortcut icon" href="img/favicon.ico" />
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/solid.css" integrity="sha384-wnAC7ln+XN0UKdcPvJvtqIH3jOjs9pnKnq9qX68ImXvOGz2JuFoEiCjT8jyZQX2z" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css" integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<meta name="theme-color" content="#bd2130">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Pizzapp</title>
+	<link rel="shortcut icon" href="img/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/solid.css" integrity="sha384-wnAC7ln+XN0UKdcPvJvtqIH3jOjs9pnKnq9qX68ImXvOGz2JuFoEiCjT8jyZQX2z" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css" integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
 
-    <link rel="stylesheet" type="text/css" href="sidebar_includes/css/b4_sidebar.css">
+	<link rel="stylesheet" type="text/css" href="sidebar_includes/css/b4_sidebar.css">
 
   </head>
   <body>
-    <div class="container-fluid">
-      <div class="row" >
-        <nav class="navbar navbar-expand-lg navbar-light bg-danger sidebarNavigation" data-sidebarClass="navbar-light bg-danger" style="width: 100%; position: absolute; z-index: 9999; background-color: #bd2130 !important;">
-              <button class="navbar-toggler leftNavbarToggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarMenu">
-                <div class="navbar-nav">
-                  <a class="nav-item nav-link" href="index.php" style="color: white;"><i class="fas fa-home"></i> Home</a>
-                  <a class="nav-item nav-link" href="cardapio.php" style="color: white;"><i class="fas fa-book-open"></i> Cardápio</a>
-                  <a class="nav-item nav-link" href="promocoes.php" style="color: white;"><i class="fas fa-dollar-sign"></i> Promoções</a>
-                  <a class="nav-item nav-link" href="cart.php" style="color: white;"><i class="fas fa-shopping-cart"></i> Meu Pedido</a>
-                  <a class="nav-item nav-link" href="entrega.php" style="color: white;"><i class="fas fa-map-marker-alt"></i> Local de Entrega</a>
-                  <a class="nav-item nav-link" href="conta.php" style="color: white;"><i class="fas fa-user-alt"></i> Minha conta</a>
-                  <a class="nav-item nav-link" href="critica.php" style="color: white;"><i class="fas fa-comment-alt"></i> Criticas e sugestões</a>
-                  <a class="nav-item nav-link" href="tel:08136341122" style="color: white;"><i class="fas fa-phone"></i> Pedir por telefone</a>
-                </div>
-             </div>
-        </nav> 
-      </div>
-      <div class="row">
-        <div class="col"></div>
-        <div class="col-md-4" id="info">
+	<div class="container-fluid">
+	  <div class="row" >
+		<nav class="navbar navbar-expand-lg navbar-light bg-danger sidebarNavigation" data-sidebarClass="navbar-light bg-danger" style="width: 100%; position: absolute; z-index: 9999; background-color: #bd2130 !important;">
+			  <button class="navbar-toggler leftNavbarToggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			  </button>
+			  <div class="collapse navbar-collapse" id="navbarMenu">
+				<div class="navbar-nav">
+				  <a class="nav-item nav-link" href="index.php" style="color: white;"><i class="fas fa-home"></i> Home</a>
+				  <a class="nav-item nav-link" href="cardapio.php" style="color: white;"><i class="fas fa-book-open"></i> Cardápio</a>
+				  <a class="nav-item nav-link" href="promocoes.php" style="color: white;"><i class="fas fa-dollar-sign"></i> Promoções</a>
+				  <a class="nav-item nav-link" href="cart.php" style="color: white;"><i class="fas fa-shopping-cart"></i> Meu Pedido</a>
+				  <a class="nav-item nav-link" href="entrega.php" style="color: white;"><i class="fas fa-map-marker-alt"></i> Local de Entrega</a>
+				  <a class="nav-item nav-link" href="conta.php" style="color: white;"><i class="fas fa-user-alt"></i> Minha conta</a>
+				  <a class="nav-item nav-link" href="critica.php" style="color: white;"><i class="fas fa-comment-alt"></i> Criticas e sugestões</a>
+				  <a class="nav-item nav-link" href="tel:08136341122" style="color: white;"><i class="fas fa-phone"></i> Pedir por telefone</a>
+				</div>
+			 </div>
+		</nav> 
+	  </div>
+	  <div class="row">
+		<div class="col"></div>
+		<div class="col-md-4" id="info">
 
-          <img src="img/logo.png" class="rounded-circle mx-auto d-block mb-5" width="40%">
+		  <img src="img/logo.png" class="rounded-circle mx-auto d-block mb-5" width="40%">
 
-          <center>
-            <h6 style="color: white; text-shadow: 1px 1px 1px black; font-weight: 400;"><i class="fas fa-phone"></i> (81) 3634-1166 / (81) 9 9915-5568</h6>
-            <h5 style="color: white; text-shadow: 1px 1px 1px black; font-weight: 400;"><i class="fas fa-map-marker-alt"></i> Surubim - PE</h5>
+		  <center>
+			<h6 style="color: white; text-shadow: 1px 1px 1px black; font-weight: 400;"><i class="fas fa-phone"></i> (81) 3634-1166 / (81) 9 9915-5568</h6>
+			<h5 style="color: white; text-shadow: 1px 1px 1px black; font-weight: 400;"><i class="fas fa-map-marker-alt"></i> Surubim - PE</h5>
 
-            <a href="cardapio.php">
-              <button type="button" class="btn btn-primary" style="width: 100%; border-radius: 0; margin-top: 15px; margin-bottom: 15px;"><i class="fas fa-shopping-cart"></i> FAÇA SEU PEDIDO</button>
-            </a>
+			<a href="cardapio.php">
+			  <button type="button" class="btn btn-primary" style="width: 100%; border-radius: 0; margin-top: 15px; margin-bottom: 15px;"><i class="fas fa-shopping-cart"></i> FAÇA SEU PEDIDO</button>
+			</a>
 
-            <span><i class="fas fa-money-bill-alt"></i> R$ 4,00 - Valor da entrega.</span><br>
-            <span><i class="fas fa-motorcycle"></i> 30Min - Tempo estimado de entrega.</span><br>
-            <span><i class="fas fa-clock"></i> Horário de atendimento: 8:00h às 23:59h de todos os dias.</span>
-          </center>
+			<span><i class="fas fa-money-bill-alt"></i> R$ 4,00 - Valor da entrega.</span><br>
+			<span><i class="fas fa-motorcycle"></i> 30Min - Tempo estimado de entrega.</span><br>
+			<span><i class="fas fa-clock"></i> Horário de atendimento: 8:00h às 23:59h de todos os dias.</span>
+		  </center>
 
-        </div>
-        <div class="col"></div>
-      </div>
-    </div>
+		</div>
+		<div class="col"></div>
+	  </div>
+	</div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
   </body>
 </html>
