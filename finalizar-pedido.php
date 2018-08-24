b1c4a649288fc3dd15cfcb64046409b525b1c5e6
Roberto <roberto10331@gmail.com>
2018-08-24 16:24:54 -0300

    Finalizar pedido
    FALTA TERMINAR.
    Salvar todos os produtos como um só pedido.

--- finalizar-pedido.php
+++ finalizar-pedido.php
@@ -15,29 +15,38 @@
 		$produto_quantidade = $mostra['temporario_quantidade'];
 		$produto_preco = $mostra['temporario_preco'];
 
-		$valor_total = ($produto_quantidade*$produto_preco);
-
+		$valor_total = ($produto_quantidade * $produto_preco);
 		$hora = date('H:i:s');
-		
-		$inserir = $pdo->prepare("INSERT INTO pedidos (produto_id, produto_nome, quantidade, preco, valor_total, hora, sessao, status) VALUES (:produto_id, :produto_nome, :quantidade, :preco, :valor_total, :hora, :sessao, :status)");
-		$inserir->bindValue(':produto_id', $produto_id);
-		$inserir->bindValue(':produto_nome', $produto_nome);
-		$inserir->bindValue(':quantidade', $produto_quantidade);
-		$inserir->bindValue(':preco', $produto_preco);
-		$inserir->bindValue(':valor_total', $valor_total);
-		$inserir->bindValue(':hora', $hora);
-		$inserir->bindValue(':sessao', $sessao);
-		$inserir->bindValue(':status', 'Aguardando atendimento');
-		$inserir->execute();
 
-		// Remove o pedido da tabela carrinho_temporario depois de ter adicionado na tabela de pedidos
-		$delete = $pdo->prepare('DELETE FROM carrinho_temporario WHERE temporario_sessao = :sessao');
-		$delete->bindValue(':sessao', $sessao);
-		$delete->execute();
+		
+		for ($i=0; $i < $linhas; $i++) { 
+			$descricao = $produto_nome.' - '.$produto_quantidade.' Unidades </br>';
+		}
+
 	}
+
+	// Inseri o pedido na tabela de pedidos
+	$inserir = $pdo->prepare("INSERT INTO pedidos (produto_id, produto_nome, descricao, quantidade, preco, valor_total, hora, sessao, status) VALUES (:produto_id, :produto_nome, :descricao, :quantidade, :preco, :valor_total, :hora, :sessao, :status)");
+	$inserir->bindValue(':produto_id', $produto_id);
+	$inserir->bindValue(':produto_nome', $produto_nome);
+	$inserir->bindValue(':descricao', $descricao);
+	$inserir->bindValue(':quantidade', $produto_quantidade);
+	$inserir->bindValue(':preco', $produto_preco);
+	$inserir->bindValue(':valor_total', $valor_total);
+	$inserir->bindValue(':hora', $hora);
+	$inserir->bindValue(':sessao', $sessao);
+	$inserir->bindValue(':status', 'Aguardando atendimento');
+	$inserir->execute();
+
+	// Remove o pedido da tabela carrinho_temporario depois de ter adicionado na tabela de pedidos
+	$delete = $pdo->prepare('DELETE FROM carrinho_temporario WHERE temporario_sessao = :sessao');
+	$delete->bindValue(':sessao', $sessao);
+	$delete->execute();
 
 	if ($inserir && $delete) {
 		echo 1;
+	}else{
+		echo 0;
 	}
 
 ?>
