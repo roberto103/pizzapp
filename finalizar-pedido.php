b321be50294a93465838843e66ddd80393486b40
Roberto <roberto10331@gmail.com>
2018-09-13 16:44:17 -0300

    Puxando dados das tabelas Usuarios e Carrinho_temporario

--- finalizar-pedido.php
+++ finalizar-pedido.php
@@ -4,12 +4,23 @@
 	include_once 'core/conexao.php';
 	
 	$sessao = $_POST['ref'];
-	$consulta = $pdo->prepare("SELECT * FROM carrinho_temporario WHERE temporario_sessao = :ses");
+
+	// $consulta = $pdo->prepare("SELECT * FROM carrinho_temporario WHERE temporario_sessao = :ses");
+	// $consulta->bindValue(':ses', $sessao);
+
+	$consulta = $pdo->prepare('SELECT c.*, u.* FROM carrinho_temporario c INNER JOIN usuarios u ON u.ID = c.ID_usuarios WHERE c.temporario_sessao = :ses');
 	$consulta->bindValue(':ses', $sessao);
+
 	$consulta->execute();
 	$linhas = $consulta->rowCount();
+
 	
 	foreach ($consulta as $mostra) {
+
+		$cliente_nome = $mostra['nome'];
+		$endereco = $mostra['endereco'];
+		$ponto_referencia = $mostra['ponto_de_referencia'];
+
 		$produto_id = $mostra['temporario_produto'];
 		$produto_nome = $mostra['temporario_nome'];
 		$produto_quantidade = $mostra['temporario_quantidade'];
@@ -23,19 +34,20 @@
 	}
 
 	// Inseri o pedido na tabela de pedidos
-	$inserir = $pdo->prepare("INSERT INTO pedidos (produto_id, produto_nome, descricao, quantidade, preco, valor_total, hora, sessao, status) VALUES (:produto_id, :produto_nome, :descricao, :quantidade, :preco, :valor_total, :hora, :sessao, :status)");
+	$inserir = $pdo->prepare("INSERT INTO pedidos (produto_id, cliente_nome, endereco, ponto_referencia, descricao, valor_total, hora, sessao, status) VALUES (:produto_id, :cliente_nome, :endereco, :ponto_referencia, :descricao, :valor_total, :hora, :sessao, :status)");
+
 	$inserir->bindValue(':produto_id', $produto_id);
-	$inserir->bindValue(':produto_nome', $produto_nome);
+	$inserir->bindValue(':cliente_nome', $cliente_nome);
+	$inserir->bindValue(':endereco', $endereco);
+	$inserir->bindValue(':ponto_referencia', $ponto_referencia);
 	$inserir->bindValue(':descricao', $descricao);
-	$inserir->bindValue(':quantidade', $produto_quantidade);
-	$inserir->bindValue(':preco', $produto_preco);
 	$inserir->bindValue(':valor_total', $valor_total);
 	$inserir->bindValue(':hora', $hora);
 	$inserir->bindValue(':sessao', $sessao);
 	$inserir->bindValue(':status', 'Aguardando atendimento');
 	$inserir->execute();
 
-	// Remove o pedido da tabela carrinho_temporario depois de ter adicionado na tabela de pedidos
+	// // // Remove o pedido da tabela carrinho_temporario depois de ter adicionado na tabela de pedidos
 	$delete = $pdo->prepare('DELETE FROM carrinho_temporario WHERE temporario_sessao = :sessao');
 	$delete->bindValue(':sessao', $sessao);
 	$delete->execute();
