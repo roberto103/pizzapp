234e3dfcb56053452355eca7a773169425419fa6
Roberto <roberto10331@gmail.com>
2018-09-18 16:42:32 -0300

    Funções / Status
    Funções foram removidas do index e colocadas no funcoes.js

--- adm/index.php
+++ adm/index.php
@@ -92,7 +92,7 @@
 										<td><?php echo $pedido->hora; ?></td>
 										<td>
 											<center>
-												<button class="btn btn-outline-primary btn-mostrar_pedido" data-toggle="modal" data-target="#modal-mostrar_pedido" data-id="<?php echo $pedido->id; ?>" data-id_pedido="<?php echo $pedido->sessao; ?>" data-valor="<?php echo decimalTela($pedido->valor_total); ?>" data-desc="<?php echo $pedido->descricao; ?>" data-hora="<?php echo $pedido->hora; ?>" data-endereco="<?php echo $pedido->endereco; ?>" data-referencia="<?php echo $pedido->ponto_referencia; ?>" data-cliente="<?php echo $pedido->cliente_nome; ?>" data-status="<?php echo $pedido->status; ?>">Ver pedido</button>
+												<button class="btn btn-outline-primary btn-mostrar_pedido" data-toggle="modal" data-target="#modal-mostrar_pedido" data-id="<?php echo $pedido->id; ?>" data-id_pedido="<?php echo $pedido->sessao; ?>" data-valor="<?php echo decimalTela($pedido->valor_total); ?>" data-desc="<?php echo $pedido->descricao; ?>" data-hora="<?php echo $pedido->hora; ?>" data-endereco="<?php echo $pedido->endereco; ?>" data-referencia="<?php echo $pedido->ponto_referencia; ?>" data-cliente="<?php echo $pedido->cliente_nome; ?>" data-status="<?php echo $pedido->status; ?>" data-sessao="<?php echo $pedido->sessao; ?>">Ver pedido</button>
 											</center>
 										</td>
 										<td>
@@ -382,9 +382,9 @@
 
 					      </div>
 					      <div class="modal-footer">
-					      	<button type="button" id="atender" class="btn btn-outline-success" disabled>Atender pedido</button>
-					      	<button type="button" id="pronto" class="btn btn-outline-success" disabled>Pedido pronto</button>
-					      	<button type="button" id="entrega" class="btn btn-outline-success" disabled>Pedido saiu para entrega</button>
+					      	<button type="button" id="bt-salvar atender" class="btn btn-outline-success" disabled>Atender pedido</button>
+					      	<button type="button" id="bt-salvar pronto" class="btn btn-outline-success" disabled>Pedido pronto</button>
+					      	<button type="button" id="bt-salvar entrega" class="btn btn-outline-success" disabled>Pedido saiu para entrega</button>
 					      </div>
 					    </div>
 					  </div>
@@ -401,196 +401,7 @@
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 		<script src="../js/jquery.mask.js"></script>
 		<script type="text/javascript" src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-
-
-		<script type="text/javascript">
-
-			// Preview da imagem do produto
-			$('#upload').change(function(){
-				const file = $(this)[0].files[0];
-				const fileReader = new FileReader();
-				fileReader.onloadend = function() {
-					$('#img').attr('src', fileReader.result);
-				}
-				fileReader.readAsDataURL(file);
-			});
-
-			$(document).ready(function(){
-
-				// Confirmação para deletar produto
-				$('a[data-confirm]').click(function(ev){
-					var href = $(this).attr('href');
-					if(!$('#confirm-delete').length){
-						$('body').append('<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog modal-dialog-centered"><div class="modal-content"><div class="modal-header bg-danger text-white">EXCLUIR PRODUTO<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body">Tem certeza de que deseja excluir esse produto?</div><div class="modal-footer"><button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button><a class="btn btn-danger text-white" id="dataComfirmOK">Excluir</a></div></div></div></div>');
-					}
-					$('#dataComfirmOK').attr('href', href);
-							$('#confirm-delete').modal({show: true});
-					return false; 
-				});
-				// Confirmação para deletar produto
-
-				// DataTable
-				$('#myTable').DataTable({
-					"language": {
-						"lengthMenu": "Mostrando _MENU_ registros por página",
-						"zeroRecords": "Nada encontrado",
-						"info": "Mostrando página _PAGE_ de _PAGES_",
-						"infoEmpty": "Nenhum registro disponível",
-						"infoFiltered": "(filtrado de _MAX_ total de registros)"
-					}
-				});
-
-				// Carregamento da página de novo produto
-				$('#prod_novo a').click(function(e){
-					e.preventDefault();
-					var href = $(this).attr('href');
-					$('#novo-produto').load(href+'#novo-produto');
-				});
-
-				// Carregamento da página de nova pizza
-				$('#nova_pizza a').click(function(e){
-					e.preventDefault();
-					var href = $(this).attr('href');
-					$('#nova-pizza').load(href+'#nova-pizza');
-				});
-
-				// Carregamento da página de nova promoção
-				$('#promo_nova a').click(function(e){
-					e.preventDefault();
-					var href = $(this).attr('href');
-					$('#nova-promocao').load(href+'#nova-promocao');
-				});
-
-				// Carregamento da página de editar pizza
-				$('#editar_pizzas a').click(function(e){
-					e.preventDefault();
-					var href = $(this).attr('href');
-					$('#nova-pizza').load(href+'#nova-pizza');
-				});
-
-				// Altera status do pedido
-				$('.btn-status').click(function(){
-
-					var sessao_pedido = $(this).attr('data-sessao_pedido');
-					var status_pedido = $('#status_pedido').val();
-
-					$.ajax({
-						url: 'core/status_pedido.php',
-						type: 'post',
-						data: {
-							status_pedido: status_pedido,
-							sessao: sessao_pedido
-						},
-						success:function(data){
-							if (data == 1) {
-								alert('Status do pedido alterado.');
-							}else{
-								alert('O status do pedido não pôde ser alterado.');
-							}
-						}
-					});
-				});
-				// 
-
-				$("#btn-salvar").click(function(){
-
-					$('.modal').modal('hide');
-
-					var id = $(this).attr('data-id');
-					var nome = $("#txtNomeProduto").val();
-					var descricao = $("#txtDescricaoProduto").val();
-					var preco = $("#txtPrecoProduto").val();
-					var img = $("#upload").val();
-					var tipo = $("#tiposProduto").val();
-
-					$.ajax({
-						url: 'editarProduto.php',
-						type : 'post',
-						data : {
-								prod: id,
-								prod_nome: nome,
-								prod_descricao: descricao,
-								prod_preco: preco,
-								prod_img: img,
-								prod_tipo: tipo
-						},
-						success : function(data){
-							 if (data == 1) {
-								$("#atualizaImg_"+id).html(img);
-								$("#atualizaTitle_"+id).html(nome);
-								$("#atualizaDesc_"+id).html(descricao);
-								$("#atualiza_"+id).html('R$ '+preco);
-							 } else {
-
-							 } 
-						}//success       
-					}); //ajax
-
-					return false;
-				}); //btn-salvar
-			});
-
-
-			// MODAL PARA EDITAR PRODUTOS
-			$('#modal-produtos').on('show.bs.modal', function (event) {
-				var button = $(event.relatedTarget) // Button that triggered the modal
-				var recipient_id = button.data('id') // Extract info from data-* attributes
-				var recipient_preco = button.data('valor')
-				var recipient_desc = button.data('descricao')
-				var recipient_upload = button.data('upload')
-				var recipient_nome = button.data('nome')
-				var recipient_tipo = button.data('tipo')
-
-				var modal = $(this)
-
-				modal.find('#txtDescricaoProduto').val(recipient_desc)
-				modal.find('#txtPrecoProduto').val(recipient_preco)
-				modal.find('#upload').val(recipient_upload)
-				modal.find('#txtNomeProduto').val(recipient_nome)
-				modal.find('#tiposProduto').val(recipient_tipo)
-			})
-
-
-			// MOSTRAR PEDIDOS
-			$('#modal-mostrar_pedido').on('show.bs.modal', function (event) {
-				var button = $(event.relatedTarget) // Button that triggered the modal
-				var recipient_id = button.data('id') // Extract info from data-* attributes
-				var recipient_preco = button.data('valor')
-				var recipient_desc = button.data('desc')
-				var recipient_hora = button.data('hora')
-				var recipient_endereco = button.data('endereco')
-				var recipient_referencia = button.data('referencia')
-				var recipient_cliente_nome = button.data('cliente')
-				var recipient_status = button.data('status')
-				// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-				// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-				var modal = $(this)
-				modal.find('.modal-title').text('Pedido número: ' + recipient_id)
-
-				modal.find('#descricao').html(recipient_desc)
-				modal.find('#valor').html('R$ ' + recipient_preco)
-				modal.find('#hora').html(recipient_hora + ' Hrs')
-				modal.find('#endereco').html('Rua ' + recipient_endereco)
-				modal.find('#referencia').html(recipient_referencia)
-				modal.find('#nomeCliente').html(recipient_cliente_nome)
-
-				if (recipient_status == 'Aguardando atendimento') {
-
-					$('#atender').removeAttr('disabled')
-
-				} else if (recipient_status == 'Pedido atendido') {
-
-					$('#pronto').removeAttr('disabled')
-
-				} else if (recipient_status == 'Pedido pronto') {
-					
-					$('#entrega').removeAttr('disabled')
-
-				}
-			})
-
-
-		</script>
+		<script src="js/funcoes.js"></script>
 
 	</body>
 </html>
