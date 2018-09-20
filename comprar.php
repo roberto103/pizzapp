7a2ad00f5490130c9a267807f202bbdcd37d4dc0
Roberto <roberto10331@gmail.com>
2018-09-20 15:26:39 -0300

    Correção da ordem

--- comprar.php
+++ comprar.php
@@ -69,8 +69,9 @@
 
 	}else{
 		$inserir = $pdo->prepare("INSERT INTO carrinho_temporario (ID_usuarios, temporario_produto, temporario_nome, temporario_quantidade, temporario_preco, temporario_img, temporario_data, temporario_sessao) VALUES (:ID_usuarios, :temporario_produto, :temporario_nome, :temporario_quantidade, :temporario_preco, :temporario_img, :temporario_data, :temporario_sessao)");
-		$inserir->bindValue(':temporario_produto', $id);
+
 		$inserir->bindValue(':ID_usuarios', $id_usuario);
+		$inserir->bindValue(':temporario_produto', $id);
 		$inserir->bindValue(':temporario_nome', $nome);
 		$inserir->bindValue(':temporario_quantidade', $quantidade);
 		$inserir->bindValue(':temporario_preco', $preco);
