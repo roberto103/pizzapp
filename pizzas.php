3a33e6db7e19b7c31f8b5d714300f42cc2bd4860
Roberto <roberto10331@gmail.com>
2018-10-04 15:14:24 -0300

    Toast adicionado

--- pizzas.php
+++ pizzas.php
@@ -26,6 +26,7 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/solid.css" integrity="sha384-wnAC7ln+XN0UKdcPvJvtqIH3jOjs9pnKnq9qX68ImXvOGz2JuFoEiCjT8jyZQX2z" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css" integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/toast.css">
 		<style type="text/css">
 			h4{color: white;}label{color: white;}
 			#img-prod{
@@ -216,6 +217,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
 		<script src="sidebar_includes/js/b4_sidebar.js"></script>
 		<script src="js/funcoes_pizza.js"></script>
 
@@ -225,13 +227,13 @@
 			$('.escolha-sabor').click(function(){
 
 				id_pizza = $(this).attr('data-id');
-				
+
 			});
 
 			$('#finalizar').click(function(){
 
-			    
 			    var valorFinal = $('#valorfinal').val();
+			    var tamanhoPizza = $('#tamanhoPizza').val();
 			    var saborPizza = $('#saborpizza').val();
 			    
 			    $.ajax({
@@ -240,9 +242,10 @@
 			      data: {
 			           id: id_pizza,
 			           valor: valorFinal,
+			           tamanho: tamanhoPizza,
 			           sabor: saborPizza
 			      },
-			      success : function(data){
+			      success: function(data){
 			         if (data == 1) {
 			         	alert('Foi');
 			            // M.toast({html: 'O Produto foi adicionado ao carrinho.'});
