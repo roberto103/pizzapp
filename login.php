e30c5dd33c4e290e771f9f9a5c311eb412c4817a
roberto103 <roberto10331@gmail.com>
2018-10-12 19:09:00 -0300

    Tela de login

--- login.php
+++ login.php
@@ -1,117 +1,112 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="theme-color" content="#bd2130">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title>Pizzapp</title>
-    <link rel="shortcut icon" href="img/favicon.ico" />
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/solid.css" integrity="sha384-wnAC7ln+XN0UKdcPvJvtqIH3jOjs9pnKnq9qX68ImXvOGz2JuFoEiCjT8jyZQX2z" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css" integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
+	<head>
+		<meta charset="utf-8">
+		<meta name="theme-color" content="#bd2130">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>Pizzapp</title>
+		<link rel="shortcut icon" href="img/favicon.ico" />
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/solid.css" integrity="sha384-wnAC7ln+XN0UKdcPvJvtqIH3jOjs9pnKnq9qX68ImXvOGz2JuFoEiCjT8jyZQX2z" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css" integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/signin.css">
 
-    <link rel="stylesheet" type="text/css" href="sidebar_includes/css/b4_sidebar.css">
+		<link rel="stylesheet" type="text/css" href="sidebar_includes/css/b4_sidebar.css">
 
-  </head>
-  <body>
-    <div class="container-fluid">
-      <div class="row" >
-        <nav class="navbar navbar-expand-lg navbar-light bg-danger sidebarNavigation" data-sidebarClass="navbar-light bg-danger" style="width: 100%; position: absolute; z-index: 9999; background-color: #bd2130 !important;">
-              <button class="navbar-toggler leftNavbarToggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarMenu">
-                <div class="navbar-nav">
-                  <a class="nav-item nav-link" href="index.php" style="color: white;"><i class="fas fa-home"></i> Home</a>
-                  <a class="nav-item nav-link" href="cardapio.php" style="color: white;"><i class="fas fa-book-open"></i> Cardápio</a>
-                  <a class="nav-item nav-link" href="promocoes.php" style="color: white;"><i class="fas fa-dollar-sign"></i> Promoções</a>
-                  <a class="nav-item nav-link" href="cart.php" style="color: white;"><i class="fas fa-shopping-cart"></i> Meu Pedido</a>
-                  <a class="nav-item nav-link" href="entrega.php" style="color: white;"><i class="fas fa-map-marker-alt"></i> Local de Entrega</a>
-                  <a class="nav-item nav-link" href="conta.php" style="color: white;"><i class="fas fa-user-alt"></i> Minha conta</a>
-                  <a class="nav-item nav-link" href="critica.php" style="color: white;"><i class="fas fa-comment-alt"></i> Criticas e sugestões</a>
-                  <a class="nav-item nav-link" href="tel:08136341122" style="color: white;"><i class="fas fa-phone"></i> Pedir por telefone</a>
-                </div>
-             </div>
-        </nav> 
-      </div>
-      <div class="row">
-        <div class="col"></div>
-        <div class="col-sm-4" id="info">
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="row" >
+				<nav class="navbar navbar-expand-lg navbar-light bg-danger sidebarNavigation" data-sidebarClass="navbar-light bg-danger" style="width: 100%; position: absolute; z-index: 9999; background-color: #bd2130 !important;">
+							<button class="navbar-toggler leftNavbarToggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+								<span class="navbar-toggler-icon"></span>
+							</button>
+							<div class="collapse navbar-collapse" id="navbarMenu">
+								<div class="navbar-nav">
+									<a class="nav-item nav-link" href="index.php" style="color: white;"><i class="fas fa-home"></i> Home</a>
+									<a class="nav-item nav-link" href="cardapio.php" style="color: white;"><i class="fas fa-book-open"></i> Cardápio</a>
+									<a class="nav-item nav-link" href="promocoes.php" style="color: white;"><i class="fas fa-dollar-sign"></i> Promoções</a>
+									<a class="nav-item nav-link" href="cart.php" style="color: white;"><i class="fas fa-shopping-cart"></i> Meu Pedido</a>
+									<a class="nav-item nav-link" href="entrega.php" style="color: white;"><i class="fas fa-map-marker-alt"></i> Local de Entrega</a>
+									<a class="nav-item nav-link" href="conta.php" style="color: white;"><i class="fas fa-user-alt"></i> Minha conta</a>
+									<a class="nav-item nav-link" href="critica.php" style="color: white;"><i class="fas fa-comment-alt"></i> Criticas e sugestões</a>
+									<a class="nav-item nav-link" href="tel:08136341122" style="color: white;"><i class="fas fa-phone"></i> Pedir por telefone</a>
+								</div>
+						 </div>
+				</nav> 
+			</div>
+			<div class="row">
+				<div class="col"></div>
+				<div class="col-sm-4" id="info">
 
-          <img src="img/logo.png" class="rounded-circle mx-auto d-block mb-3" width="33%">
-          
-        	<form method="post">
+					<img src="img/logo.png" class="rounded-circle mx-auto d-block mb-3" width="40%">
 
-    			  <div class="form-group">
-    			    <label for="email" style="color: white;">Email</label>
-    			    <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Digite seu email." style="border-radius: 0px;">
-    			  </div>
+					<form class="form-signin">
+						<label for="email" class="sr-only">Email</label>
+						<input type="email" id="email" class="form-control" placeholder="Digite seu email." required autofocus>
 
-    			  <div class="form-group">
-    			    <label for="senha" style="color: white;">Senha</label>
-    			    <input type="password" name="senha" class="form-control" id="senha" placeholder="Digite sua senha." style="border-radius: 0px;">
-    			  </div>
+						<label for="senha" class="sr-only">Senha</label>
+						<input type="password" id="senha" class="form-control" placeholder="Digite sua senha." required>
 
-    			  <div class="custom-control custom-checkbox">
-      				<input type="checkbox" class="custom-control-input" id="customCheck1">
-     			 	  <label class="custom-control-label" for="customCheck1" style="color: white;">Mantenha-me conectado</label>
-    			  </div>
+						<div style="display: none;" class="alert alert-danger" role="alert">
+							Email ou senha incorretos!
+						</div>
 
-    			  <hr class="mb-3">
+						<button class="btn btn-lg btn-primary btn-block mb-4" id="btLogin" type="submit">Logar</button>
 
-    			  <button id="btLogin" type="submit" class="btn btn-primary mb-3" style="width: 100%; border-radius: 0;"><i class="fas fa-check-circle"></i> Login</button>
+						<center>
+							<a href="cadastro.php" class="text-white text-center">Cadastre-se agora</a>
+						</center>
+					</form>
 
-            <a href="cadastro.php" class="btn btn-primary mb-3" style="width: 100%; border-radius: 0;">Cadastrar</a>
-    			</form>
+				</div>
+				<div class="col"></div>
+			</div>
+		</div>
 
-        </div>
-        <div class="col"></div>
-      </div>
-    </div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		<script src="sidebar_includes/js/b4_sidebar.js"></script>
+		
+		<script type="text/javascript">
+			
+			$(document).ready(function(){
+			 
+				
+				$('#btLogin').click(function(){
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="sidebar_includes/js/b4_sidebar.js"></script>
-    
-    <script type="text/javascript">
-      
-      $(document).ready(function(){
-       
-        
-        $('#btLogin').click(function(){
+						var usuario = $("#email").val();
+						var senha = $("#senha").val();
+						
+						$.ajax({
+							url : "auth/validar.php",
+							type : 'post',
+							data : {
+									 email : usuario,
+									 senha :senha
+							},
+							success : function(data){
+								 if (data == 1) {
+										window.location = 'cardapio.php';
+								 }else if(data == 2){
+										alert('Os campos não podem estar vazios');
+								 } else {
+										$('.alert-danger').css('display', 'block');
+								 } 
+							}//success          
+						});//ajax
 
-            var usuario = $("#email").val();
-            var senha = $("#senha").val();
-            
-            $.ajax({
-              url : "auth/validar.php",
-              type : 'post',
-              data : {
-                   email : usuario,
-                   senha :senha
-              },
-              success : function(data){
-                 if (data == 1) {
-                    window.location = 'cardapio.php';
-                 }else if(data == 2){
-                    alert('Os campos não podem estar vazios');
-                 } else {
-                    alert('Senha ou email incorretos!');
-                 } 
-              }//success          
-            });//ajax
+						return false;
 
-            return false;
-
-        });//#btLogin
+				});//#btLogin
 
 
 
-      });//documento.ready
+			});//documento.ready
 
-    </script>
-  </body>
+		</script>
+	</body>
 </html>
