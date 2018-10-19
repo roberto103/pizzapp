505314b0c6e57b59ca049a8a6c02c3a3e325c9ed
Roberto <roberto10331@gmail.com>
2018-10-19 16:07:32 -0300

    Menu removido

--- bebidas.php
+++ bebidas.php
@@ -31,7 +31,6 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css" integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/toast.css">
 
-    <!-- <script type="text/javascript" src="sidebar_includes/js/b4_sidebar.js"></script> -->
 
   </head>
   <body>
@@ -77,7 +76,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <!-- <script type="text/javascript" src="sidebar_includes/js/b4_sidebar.js"></script> -->
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
 
