03b30c46b5a6e3b58040ffe4cb8b73e738431c5b
Roberto <roberto10331@gmail.com>
2018-10-23 13:39:55 -0200

    Banco de imagem adiconado

--- adm/editar_pizza.php
+++ adm/editar_pizza.php
@@ -21,11 +21,12 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/fontawesome.css" integrity="sha384-GVa9GOgVQgOk+TNYXu7S/InPTfSDTtBalSgkgqQ7sCik56N9ztlkoTr2f/T44oKV" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/solid.css" integrity="sha384-Rw5qeepMFvJVEZdSo1nDQD5B6wX0m7c5Z/pLNvjkB14W6Yki1hKbSEQaX9ffUbWe" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 
 <div class="container-fluid">
 
 	<div class="row" >
-		<div class="col-sm-12" style="background-color: #ff8a00 !important; height: 60px; position: absolute; z-index: 9999;">
+		<div class="col-sm-12" style="background-color: #ff8a00 !important; height: 60px; position: absolute; z-index: 998;">
 		</div> 
 	</div>
 	<div class="row mb-4">
@@ -98,34 +99,60 @@
 					<textarea class="form-control" id="txtDescricaoPizza" name="txtDescricaoPizza" placeholder="Descrição da pizza"><?php echo $p->descricao; ?></textarea>
 				</div>
 
-				<div class="form-group">
+				<div class="form-group" id="form-group_pizzas">
 					<label for="upload" style="display: block;">Imagem da pizza</label>
-					<img id="img_pizza" style="width: 15%">
-					<input type="file" class="form-control" name="img_pizza" id="upload_pizza">
+
+					<img class="d-block img mb-4" id="img" style="width: 15%">
+
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalImagensPizzas">Selecionar imagem</button>
 				</div>
 
-				<input type="hidden" name="img_pizza-salva" value="<?php echo $p->img_pizza; ?>">
+				<input class="form-control" type="hidden" name="img_pizza" id="upload">
 
-				<button type="submit" class="btn btn-outline-success" style="width: 100%;">Salvar</button>
+				<button type="submit" class="btn btn-outline-success" style="width: 100%;">Adicionar</button>
 			</form>
 
 		</div>
 	</div>
 </div>
 
+<!-- MODAL DAS IMAGENS PIZZAS -->
+<div class="modal fade bd-example-modal-lg" id="modalImagensPizzas" tabindex="-1" role="dialog" aria-labelledby="modallabelEx" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="modallabelEx">Banco de imagens</h5>
+			</div>
+
+			<div class="modal-body">
+				<div class="imagens">
+				<?php 
+					$pasta = '../img/produtos/pizzas/';
+					$arquivos = scandir($pasta);
+
+					foreach ($arquivos as $arquivo) {
+						if (($arquivo != '.') && ($arquivo != '..')) {
+				 ?>
+					<img data-dismiss="modal" src="<?php echo $pasta.$arquivo; ?>" class="img-fluid img-pizzas" data-nome="<?php echo $arquivo;?>">
+				<?php }} ?>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- /MODAL DAS IMAGENS PIZZAS-->
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script type="text/javascript" src="../js/jquery.mask.js"></script>
 
 <script type="text/javascript">
 
-	$('.money2').mask("0.000,00", {reverse: true});
-
-	$('#upload_pizza').change(function(){
-		const file = $(this)[0].files[0];
-		const fileReader = new FileReader();
-		fileReader.onloadend = function() {
-			$('#img_pizza').attr('src', fileReader.result);
-		}
-		fileReader.readAsDataURL(file);
+	$('.img-pizzas').click(function(){
+		var img_nome = $(this).attr('data-nome');
+		$('.img').attr('src', '../img/produtos/pizzas/'+img_nome);
+		$('#upload').val('pizzas/'+img_nome)
 	});
+
 </script>
